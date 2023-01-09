c950a19adf28fedc52ee9542a92d0ef9404f5487
AF-Jama <abdifatah.jama01@protonmail.com>
2023-01-09 03:04:26 +0000

    Removed unnecessary temp file

--- temp.js
+++
@@ -1,5 +0,0 @@
-const { pbkdf2Sync } = require('pbkdf2');
-
-
-const a = pbkdf2Sync('c174b18fa4553cb97a14c5d6841a8461b3e782a2fa8dfbefd8351463580ec255','qusek',1000,32,'sha512');
-console.log(a.toString('hex'));
