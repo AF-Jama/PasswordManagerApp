781a9ec891ba89750dceec6b099618fe26290fc4
AF-Jama <abdifatah.jama01@protonmail.com>
2023-02-14 14:28:41 +0000

    .

--- server/app.js
+++ server/app.js
@@ -35,6 +35,15 @@ app.use(cors({
   exposedHeaders: ['*', 'Authorization','set-cookie'] 
 
 }))
+
+app.use(function(req, res, next) {
+  res.header('Access-Control-Allow-Credentials', true);
+  res.header('Access-Control-Allow-Origin', "http://passwordmanagerbucket.s3-website-us-east-1.amazonaws.com");
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+  res.header('Access-Control-Allow-Headers', 'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept');
+  next();
+});
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/index', indexRouter);
